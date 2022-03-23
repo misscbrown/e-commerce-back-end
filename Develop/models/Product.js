2059792a726d1f5a0f0e67c07ff51b11a16d4876
cee breezy <djmisscbrown@hotmail.com>
2022-03-23 19:18:57 +0000

    Added id, name and price

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,6 +9,24 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    
     // define columns
   },
   {
