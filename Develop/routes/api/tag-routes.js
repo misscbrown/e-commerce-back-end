b87a4fb870483a82c4f64407f5ac04bcccfeaa80
cee breezy <djmisscbrown@hotmail.com>
2022-03-23 20:33:25 +0000

    updated router.post, put and delete functions

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -49,6 +49,56 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(req.body)
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
 
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body,{
+    where:
+    {
+      id:req.params.id
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where:
+    {
+      id:req.params.id
+    }
+  })
+  .then(data =>
+  {
+    res.json(data);
+  })
+  .catch(err => 
+  {
+    console.log(err);
+    res.status(400).json(err);
+  });
+});
 
 module.exports = router;
